b213ef1d90638b029b951093a534ddeaa1df35b0
hoseacodes <dominique11h@yahoo.com>
2021-07-23 23:38:39 -0500

    Added Lessons and About Page

---
+++ functions.js
@@ -0,0 +1,16 @@
+// Write a function named computeAreausing the function declaration approach.
+// It will have two parameters: width& height.
+// It will compute the area of a rectangle (width X height) and return a string in the following form:
+// The area of a rectangle with a width of _ and a height of _ is ___ square units.
+// Invoke the function to test it.
+
+
+
+
+  // Write a Function Expression
+  // Write a function named planetHasWaterusing the function expression syntax.
+  // It will have one parameter: planet.
+  // Return trueif the planetargument is either “Earth” or “Mars”, otherwise return false.
+  // Bonus points if you ensure the function will work regardless of the casing of the planetbeing passed in (‘earth’, ‘MARS’, etc.).
+  // Invoke the function a couple of times to test it!
+  
